dc49f73866f8448fd9f3d4d588ef13a8b79a1314
ritwik <ritwik6927@gmail.com>
2024-09-21 13:21:20 +0530

    redploy

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ require('dotenv').config();
 const route=require("./route/route")
 const connection=require("./database/db")
 
-
+const path =require("path")
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use("/", route);
 const port = process.env.PORT;
-app.use(express.static('views'));
+app.use(express.static(path.join(__dirname,'views')));
+app.get("/",(req,res)=>{
+  res.sendFile(path.join(__dirname,"views","index.html"))
+})
 
 const nitterBaseUrls = [
   'https://nitter.privacydev.net/',
